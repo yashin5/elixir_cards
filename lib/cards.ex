e91fb6f5bdb5ac260383dee3e99fe32f5762af53
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-02-15 22:06:56 -0200

    refat file_read with pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -26,11 +26,10 @@ defmodule Cards do
   end
 
   def load_deck(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-    :ok -> :erlang.binary_to_term(binary)
-    :error -> "That file does not exist"
+    case File.read(filename) do
+    {:ok, binary} -> :erlang.binary_to_term(binary)
+    {:error, reason} -> "That file does not exist"
     end
   end
+  
 end
